be8f67a51737457310e3288518849c412d41157e
Robin K. Hansen <kaaveland@gmail.com>
2008-12-17 20:47:40 +0800

    Ported munin.mod.prod to merlin. Added a cli.
    The cli is not working at the moment due to a circular import in Core
    that prevents merlin from being imported.
    
    Signed-off-by: ellonweb <ellonweb@hotmail.com>

---
+++ Hooks/ships/prod.py
@@ -0,0 +1,49 @@
+"""
+Used for calculating production times of ships.
+"""
+
+import math
+import re
+
+from .Core.modules import M
+loadable = M.loadable.loadable
+from Hooks.ships import feud
+
+class prod(loadable):
+    """Calculate ticks it takes to produce <number> <ships> with <factories>."""
+    def __init__(self):
+
+        loadable.__init__(self)
+        self.paramre = re.compile(r"\s(\d+[km]?)\s(\S+)\s(\d+)")
+        self.usage += " <number> <ship> <factories>."
+
+    def execute(self, message):
+
+        user, params = loadable.execute(self, message) or (None, None)
+        if params is None:
+            return
+        num, name, factories = params.groups()
+
+        ship = M.DB.Maps.Ship.load(name)
+        if ship is None:
+            message.alert("%s is not a ship." % name)
+        num = self.short2num(num)
+        factories = int(factories)
+
+        ticks, feud_ticks = self.calc_ticks(ship, num, factories)
+
+        message.reply("It will take %s ticks to build %s %s, or with feudalism %s ticks." % (
+                ticks, self.num2short(num), ship.name, feud_ticks))
+
+    def calc_ticks(self, ship, num, factories):
+        """Calculate the cost in ticks. Return (ticks, ticks_with_feudalism)."""
+
+        cost = number * ship.total_cost
+        ln = lambda x: math.log(x) / math.log(math.e)
+        required = 2 * math.sqrt(cost) * ln(cost)
+        feud = 2 * math.sqrt(cost * 0.85) * ln(cost)
+        output = int((4000 * factories) ** 0.98)
+        ticks = int(math.ceil((required + 10000 * factories) / output))
+        feud_ticks = int(1.2 * math.ceil((feud + 10000 * factories) / output))
+
+        return ticks, feud_ticks
---
+++ cli.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+"""
+This module is intended for command-line testing.
+
+Usage:
+When running the program, the argv is used as a prefix
+for every line sent to the bot, and the output is
+written to stdout. That's to say, if I call the program like this:
+./cli-bot.py :~qebab@qebab.users.netgamers.org PRIVMSG #ascendancy :
+It will hang on stdin for input, and when it gets a line it will concatenate
+the input with this, so that for instance:
+./cli-bot.py :~qebab@qebab.users.netgamers.org PRIVMSG #ascendancy :
+> !eff 3k harpy
+The line is run as though it was:
+:~qebab@qebab.users.netgamers.org PRIVMSG #ascendancy :!eff 3k harpy
+"""
+
+import merlin
+import Core.connection as connection
+import sys
+
+class PhonyConnection(connection.Connection):
+    """Used for testing purposes."""
+    
+    def __init__(self, serv, port):
+
+        self.serv = serv
+        self.port = port
+
+    def connect(self):
+        
+        return
+
+    def write(self, line):
+
+        print line
+
+    def read(self):
+
+        sys.stdout.write("> ")
+        return " ".join(sys.argv[1:]) + sys.stdin.readline()
+    
+class Cli(merlin.Bot):
+
+    def __init__(self):
+
+        self.conn = PhonyConnection()
+
+if __name__ == "__main__":
+
+    Cli().run()
