7080b2cc685c768441affe5dd21a1fcf7ab3f1f1
ellonweb <ellonweb@hotmail.com>
2009-03-31 19:30:26 +0100

    Added @loadable.runcop to match @loadable.run

--- Core/loadable.py
+++ Core/loadable.py
@@ -77,11 +77,11 @@ class loadable(object):
     
     @staticmethod
     def runcop(f):
-        def execute(self, message):
+        def robocop(self, message):
             params = loadable.robocop(self, message)
             if params:
                 f(self, message, params)
-        return execute
+        return robocop
     
     def execute(self, message):
         m = self.commandre.search(message.get_msg())
