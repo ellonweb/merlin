156fb3d9005428e68324824f621f96a60243eac5
ellonweb <ellonweb@hotmail.com>
2009-10-13 17:59:40 +0100

    refactor prop show, add prop cancel

--- Hooks/propsandcookies/prop.py
+++ Hooks/propsandcookies/prop.py
@@ -64,10 +64,11 @@ class prop(loadable):
             if not prop.active:
                 reply+= " This prop expired %d days ago."%((now-prop.closed).days,)
             
-            veto = prop.votes.filter_by(vote="veto").all()
-            if len(veto > 0):
-                reply+= " Vetoing: "
-                reply+= ", "join(map(lambda x: x.voter.name, veto))
+            if prop.active:
+                veto = prop.votes.filter_by(vote="veto").all()
+                if len(veto > 0):
+                    reply+= " Vetoing: "
+                    reply+= ", "join(map(lambda x: x.voter.name, veto))
             
             message.reply(reply)
             
@@ -83,35 +84,30 @@ class prop(loadable):
                 message.alert(reply)
             
             if not prop.active:
-                yes = session.query(sum(Vote.carebears)).filter_by(prop_id=id, vote="yes").scalar()
-                no = session.query(sum(Vote.carebears)).filter_by(prop_id=id, vote="no").scalar()
-                veto = session.query(sum(Vote.carebears)).filter_by(prop_id=id, vote="veto").scalar()
-                reply = "The prop"
-                if veto > 0:
-                    reply+= " failed because of %s vetos"%(veto)
-                elif yes > no:
-                    reply+= " passed by a vote of %s to %s"%(yes,no)
-                else:
-                    reply+= " failed by a vote of %s to %s"%(no,yes)
-                
-                pretty_print=lambda x:"%s (%s)"%(x.voter.name,x.carebears)
-                reply+= ". The voters in favor were ("
-                reply+= ", ".join(map(pretty_print,prop.votes.filter_by(vote="yes")))
-                reply+= ") and against ("
-                reply+= ", ".join(map(pretty_print,prop.votes.filter_by(vote="no")))
-                reply+= ")."
-                
-                if veto > 0:
-                    reply+= " Vetoing ("
-                    reply+= ", ".join([vote.user.name for vote in prop.votes.filter_by(vote="veto")])
-                    reply+= ")."
-                
+                reply = self.text_result(prop)
+                reply+= self.text_summary(prop)
                 message.reply(reply)
         
         elif mode == "expire":
             pass
+        
         elif mode == "cancel":
-            pass
+            id = params.group(2)
+            prop = self.load_prop(id)
+            if prop is None:
+                message.reply("No proposition number %s exists."%(id,))
+                return
+            
+            if prop.proposer is not user and not user.is_admin():
+                message.reply("Only %s may expire proposition %d."%(prop.proposer.name,id))
+                return
+            
+            reply = "Cancelled proposal %s to %s %s" %(prop.id,prop.type,prop.person,)
+            reply+= self.text_summary(prop)
+            
+            self.delete_prop(prop)
+            message.privmsg(reply, Config.get("Channels","home"))
+        
         elif mode == "vote":
             pass
         
@@ -138,6 +134,44 @@ class prop(loadable):
         invite = session.query(Invite).filter_by(id=id).first()
         kick = session.query(Kick).filter_by(id=id).first()
         return invite or kick
+    
+    def sum_votes(self, prop):
+        yes = session.query(sum(Vote.carebears)).filter_by(prop_id=prop.id, vote="yes").scalar()
+        no = session.query(sum(Vote.carebears)).filter_by(prop_id=prop.id, vote="no").scalar()
+        veto = session.query(sum(Vote.carebears)).filter_by(prop_id=prop.id, vote="veto").scalar()
+        return yes, no, veto
+    
+    def text_result(self, prop):
+        yes, no, veto = self.sum_votes(prop)
+        reply = "The prop"
+        if veto > 0:
+            reply+= " failed because of %s vetos"%(veto)
+        elif yes > no:
+            reply+= " passed by a vote of %s to %s"%(yes,no)
+        else:
+            reply+= " failed by a vote of %s to %s"%(no,yes)
+        return reply
+    
+    def text_summary(self, prop):
+        pretty_print=lambda x:"%s (%s)"%(x.voter.name,x.carebears)
+        reply = ". The voters in favor were ("
+        reply+= ", ".join(map(pretty_print,prop.votes.filter_by(vote="yes")))
+        reply+= ") and against ("
+        reply+= ", ".join(map(pretty_print,prop.votes.filter_by(vote="no")))
+        reply+= ")."
+        
+        veto = prop.votes.filter_by(vote="veto").all()
+        if len(veto) > 0:
+            reply+= " Vetoing ("
+            reply+= ", ".join([vote.user.name for vote in veto])
+            reply+= ")."
+        
+        return reply
+    
+    def delete_prop(self, prop):
+        prop.votes.delete()
+        session.delete(prop)
+        session.commit()
     
     def base_prop_search(self):
         invites = session.query(Invite.id, Invite.person, Invite.vote_result, literal("invite")).filter_by(active=False)
