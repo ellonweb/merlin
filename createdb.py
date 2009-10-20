2b1c7a1f639323491c691c46f0f5bcb80abeaaaf
ellonweb <ellonweb@hotmail.com>
2009-10-21 00:52:38 +0100

    createdb now with added migration, pending sqla bug fix

--- createdb.py
+++ createdb.py
@@ -19,21 +19,54 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
+import sys
+if len(sys.argv) > 2 and sys.argv[1] == "--migrate":
+    round = sys.argv[2]
+else:
+    round = None
+    print "To migrate from an old round use: createdb.py --migrate <previous_round>"
+
+from sqlalchemy.sql import text, bindparam
+from Core.db import session
+if round:
+    print "Moving tables to '%s' schema"%(round,)
+    session.execute(text("ALTER SCHEMA public RENAME TO %s;" % (round,)))
+    session.execute(text("CREATE SCHEMA public;"))
+    session.commit()
+    session.close()
+
+from Core.db import Base
 print "Importing database models"
-from Core.db import Base, session
 from Core.maps import Channel
 
 print "Creating tables"
 Base.metadata.create_all()
 
-print "Setting up home channel"
+print "Setting up default channels"
 from Core.config import Config
-session.add(Channel(name=Config.get("Channels","home"),userlevel=100,maxlevel=1000))
+for chan, name in Config.items("Channels"):
+    session.add(Channel(name=name,userlevel=100,maxlevel=1000))
 session.commit()
 session.close()
 
-# in future add migrate code here
-# will require a second engine for the old db
+if round:
+    print "Migrating users/friends"
+    session.execute(text("INSERT INTO users (id, name, alias, passwd, active, access, email, phone, pubphone, sponsor, quits, available_cookies, carebears, last_cookie_date) SELECT id, name, alias, passwd, active, access, email, phone, pubphone, sponsor, quits, available_cookies, carebears, last_cookie_date FROM %s.users;" % (round,)))
+    session.execute(text("SELECT setval('users_id_seq',(SELECT max(id) FROM users));"))
+    session.execute(text("INSERT INTO phonefriends (user_id, friend_id) SELECT user_id, friend_id FROM %s.phonefriends;" % (round,)))
+    print "Migrating slogans/quotes"
+    session.execute(text("INSERT INTO slogans (text) SELECT text FROM %s.slogans;" % (round,)))
+    session.execute(text("INSERT INTO quotes (text) SELECT text FROM %s.quotes;" % (round,)))
+    print "Migrating props/votes/cookies"
+    session.execute(text("INSERT INTO invite_proposal (id,active,proposer_id,person,created,closed,vote_result,comment_text) SELECT id,active,proposer_id,person,created,closed,vote_result,comment_text FROM %s.invite_proposal;" % (round,)))
+    session.execute(text("INSERT INTO kick_proposal (id,active,proposer_id,person_id,created,closed,vote_result,comment_text) SELECT id,active,proposer_id,person_id,created,closed,vote_result,comment_text FROM %s.kick_proposal;" % (round,)))
+    session.execute(text("SELECT setval('proposal_id_seq',(SELECT max(id) FROM (SELECT id FROM invite_proposal UNION SELECT id FROM kick_proposal)));"))
+    session.execute(text("INSERT INTO prop_vote (vote,carebears,prop_id,voter_id) SELECT vote,carebears,prop_id,voter_id FROM %s.prop_vote;" % (round,)))
+    session.execute(text("INSERT INTO cookie_log (log_time,year,week,howmany,giver_id,receiver_id) SELECT log_time,year,week,howmany,giver_id,receiver_id FROM %s.cookie_log;" % (round,)))
+    print "Migrating smslog"
+    session.execute(text("INSERT INTO sms_log (sender_id,receiver_id,phone,sms_text) SELECT sender_id,receiver_id,phone,sms_text FROM %s.sms_log;" % (round,)))
+    session.commit()
+    session.close()
 
 print "Inserting ship stats"
 import shipstats
