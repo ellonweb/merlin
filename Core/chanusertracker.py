9ecc7f8470cb0b7a89203b4b269729c5f9a24db7
ellonweb <ellonweb@hotmail.com>
2009-11-21 23:58:45 +0000

    inital CUT core refactor

--- Core/chanusertracker.py
+++ Core/chanusertracker.py
@@ -24,15 +24,85 @@
 
 from Core.exceptions_ import PNickParseError, UserError
 from Core.config import Config
-from Core import maps
+from Core.maps import User
 
-Channels = {}
-Nicks = {}
-Users = {}
+class ChanUserTracker(object):
+    Channels = {}
+    Nicks = {}
+    Pusers = {}
+    
+    def auth_user(self, name, pnickf, username, password):
+        # Trying to authenticate with !letmein or !auth
+        nick = self.Nicks.get(name)
+        if (nick is not None) and (nick.puser is not None):
+            # They already have a user associated
+            return None
+        
+        try:
+            pnick = pnickf()
+            # They have a pnick, so shouldn't need to auth, let's auth them anyway
+            user = User.load(name=pnick)
+        except PNickParseError:
+            # They don't have a pnick, expected
+            user = User.load(name=username, passwd=password)
+        
+        if user is None:
+            raise UserError
+        
+        if (nick is not None) and (Config.get("Misc","usercache") in ("join", "command",)):
+            if self.Pusers.get(user.name) is None:
+                # Add the user to the tracker
+                self.Pusers[user.name] = Puser(user.name)
+            
+            if nick.puser is None:
+                # Associate the user and nick
+                nick.puser = self.Pusers[user.name]
+                nick.puser.nicks.add(nick)
+        
+        # Return the SQLA User
+        return user
+    
+    def get_user(self, name, pnick=None, pnickf=None):
+        # Regular user check
+        if (pnick is None) and (pnickf is None):
+            # This shouldn't happen
+            return None
+        
+        nick = self.Nicks.get(name)
+        
+        if (nick and nick.puser) is not None:
+            # They already have a user associated
+            pnick = nick.puser.name
+        elif pnickf is not None:
+            # Call the pnick function, might raise PNickParseError
+            try:
+                pnick = pnickf()
+            except PNickParseError:
+                return None
+        
+        user = User.load(name=pnick)
+        if user is None:
+            return None
+        
+        if (nick is not None) and (Config.get("Misc","usercache") in ("join", "command",)):
+            if self.Pusers.get(user.name) is None:
+                # Add the user to the tracker
+                self.Pusers[user.name] = Puser(user.name)
+            
+            if nick.puser is None:
+                # Associate the user and nick
+                nick.puser = self.Pusers[user.name]
+                nick.puser.nicks.add(nick)
+        
+        # Return the SQLA User
+        return user
+
+CUT = ChanUserTracker()
 
 class Channel(object):
     # The channel object provides a means for keeping track of a channel
-    def __init__(self, chan):
+    def __init__(self, CUT, chan):
+        self.CUT = CUT
         self.chan = chan
         self.nicks = set()
         self.topic = ""
@@ -40,22 +110,22 @@ class Channel(object):
     
     def addnick(self, name):
         # Add a new nick to the channel
-        nick = Nicks.get(name)
+        nick = self.CUT.Nicks.get(name)
         if nick is None:
             nick = Nick(name)
-            Nicks[name] = nick
+            self.CUT.Nicks[name] = nick
         self.nicks.add(nick)
         nick.channels.add(self.chan)
     
     def remnick(self, name):
         # Remove a nick from the list
-        if name not in Nicks.keys():
+        if name not in self.CUT.Nicks.keys():
             return
-        nick = Nicks[name]
+        nick = self.CUT.Nicks[name]
         self.nicks.remove(nick)
         nick.channels.remove(self.chan)
         if len(nick.channels) == 0:
-            del Nicks[nick.name]
+            del self.CUT.Nicks[nick.name]
     
     def __del__(self):
         # We've parted or been kicked, update nicks
@@ -63,7 +133,7 @@ class Channel(object):
             nick.channels.remove(self.chan)
             if len(nick.channels) == 0:
                 try:
-                    del Nicks[nick.name]
+                    del self.CUT.Nicks[nick.name]
                 # Might occur when the bot is quitting
                 except (AttributeError, KeyError, TypeError):
                     pass
@@ -71,105 +141,39 @@ class Channel(object):
 
 class Nick(object):
     # Class used for storing nicks
-    def __init__(self, nick):
+    def __init__(self, CUT, nick):
+        self.CUT = CUT
         self.name = nick
         self.channels = set()
-        self.user = None
+        self.puser = None
     
     def nick(self, name):
         # Update the nicks list
-        del Nicks[self.name]
-        Nicks[name] = self
+        del self.CUT.Nicks[self.name]
+        self.CUT.Nicks[name] = self
         self.name = name
     
     def quit(self):
         # Quitting
         for channel in self.channels.copy():
-            Channels[channel].remnick(self.name)
+            self.CUT.Channels[channel].remnick(self.name)
     
     def __del__(self):
         if self.user is not None:
             try:
-                self.user.nicks.remove(self)
-                if len(self.user.nicks) == 0:
-                    del Users[self.user.name]
+                self.puser.nicks.remove(self)
+                if len(self.puser.nicks) == 0:
+                    del self.CUT.Pusers[self.puser.name]
             # Might occur when the bot is quitting
             except (AttributeError, KeyError, TypeError):
                 pass
     
 
-class User(object):
+class Puser(object):
     # Class for storing user information for the tracker
     # Not to be confused with the SQLA class!
-    def __init__(self, name):
+    def __init__(self, CUT, name):
+        self.CUT = CUT
         self.name = name
         self.nicks = set()
-    
-
-def auth_user(name, pnickf, username, password):
-    # Trying to authenticate with !letmein or !auth
-    nick = Nicks.get(name)
-    if (nick is not None) and (nick.user is not None):
-        # They already have a user associated
-        return None
-    
-    try:
-        pnick = pnickf()
-        # They have a pnick, so shouldn't need to auth, let's auth them anyway
-        user = maps.User.load(name=pnick)
-    except PNickParseError:
-        # They don't have a pnick, expected
-        user = maps.User.load(name=username, passwd=password)
-    
-    if user is None:
-        raise UserError
-    
-    if (nick is not None) and (Config.get("Misc","usercache") in ("join", "command",)):
-        if Users.get(user.name) is None:
-            # Add the user to the tracker
-            Users[user.name] = User(user.name)
-        
-        if nick.user is None:
-            # Associate the user and nick
-            nick.user = Users[user.name]
-            nick.user.nicks.add(nick)
-    
-    # Return the SQLA User
-    return user
-    
-
-def get_user(name, pnick=None, pnickf=None):
-    # Regular user check
-    if (pnick is None) and (pnickf is None):
-        # This shouldn't happen
-        return None
-    
-    nick = Nicks.get(name)
-    
-    if (nick and nick.user) is not None:
-        # They already have a user associated
-        pnick = nick.user.name
-    elif pnickf is not None:
-        # Call the pnick function, might raise PNickParseError
-        try:
-            pnick = pnickf()
-        except PNickParseError:
-            return None
-    
-    user = maps.User.load(name=pnick)
-    if user is None:
-        return None
-    
-    if (nick is not None) and (Config.get("Misc","usercache") in ("join", "command",)):
-        if Users.get(user.name) is None:
-            # Add the user to the tracker
-            Users[user.name] = User(user.name)
-        
-        if nick.user is None:
-            # Associate the user and nick
-            nick.user = Users[user.name]
-            nick.user.nicks.add(nick)
-    
-    # Return the SQLA User
-    return user
     
