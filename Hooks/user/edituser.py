0042d37f9548551b5e6b3e449034bfc33944eb76
ellonweb <ellonweb@hotmail.com>
2009-12-20 20:49:47 +0000

    bunch of typos

--- Hooks/user/edituser.py
+++ Hooks/user/edituser.py
@@ -43,7 +43,7 @@ class edituser(loadable):
             except Exception:
                 message.reply("Invalid access level '%s'" % (access,))
                 return
-        elif access.isdigit()
+        elif access.isdigit():
             access = int(access)
         elif access in self.true:
             access = True
@@ -63,18 +63,18 @@ class edituser(loadable):
         home = Config.get("Channels","home")
         
         if type(access) == int:
-            if member.active = True and member.access < mbraxx and access >= mbraxx:
+            if member.active == True and member.access < mbraxx and access >= mbraxx:
                 message.privmsg("adduser %s %s 399" %(home, member.name,), "P")
                 message.reply("%s has been added to %s"%(member.name, home,))
-            if member.active = True and member.access >= mbraxx and access < mbraxx:
+            if member.active == True and member.access >= mbraxx and access < mbraxx:
                 message.privmsg("remuser %s %s"%(home, member.name,), "P")
                 message.privmsg("ban %s *!*@%s.users.netgamers.org GTFO, EAAD"%(home, member.name,), "P")
             member.access = access
         else:
-            if member.active != access and access = True and member.access >= mbraxx:
+            if member.active != access and access == True and member.access >= mbraxx:
                 message.privmsg("adduser %s %s 399" %(home, member.name,), "P")
                 message.reply("%s has been added to %s"%(member.name, home,))
-            if member.active != access and access = False and member.access >= mbraxx:
+            if member.active != access and access == False and member.access >= mbraxx:
                 message.privmsg("remuser %s %s"%(home, member.name,), "P")
                 message.privmsg("ban %s *!*@%s.users.netgamers.org GTFO, EAAD"%(home, member.name,), "P")
             member.active = access
