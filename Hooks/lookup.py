b4dc80b99c79cee15c6ca585be58d5beea71fa8c
ellonweb <ellonweb@hotmail.com>
2010-02-22 16:03:22 +0000

    rewrote lookup to use multiple routes

--- Hooks/lookup.py
+++ Hooks/lookup.py
@@ -19,66 +19,59 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from Core.exceptions_ import PNickParseError
 from Core.db import session
 from Core.maps import Galaxy, Planet, Alliance, User
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module()
 class lookup(loadable):
-    """"""
     usage = " [x:y[:z]|alliance|user]"
-    paramre = (loadable.coordre, re.compile(r"(?:\s(\S+))?"),)
     
-    def execute(self, message, user, params):
-        
-        # Planet or Galaxy
-        if len(params.groups()) == 5:
-            # Planet
-            if params.group(5) is not None:
-                planet = Planet.load(*params.group(1,3,5))
-                if planet is None:
-                    message.reply("No planet with coords %s:%s:%s found" % params.group(1,3,5))
-                    return
-                message.reply(str(planet))
+    @route(loadable.coord)
+    def planet_galaxy(self, message, user, params):
+        # Planet
+        if params.group(5) is not None:
+            planet = Planet.load(*params.group(1,3,5))
+            if planet is None:
+                message.reply("No planet with coords %s:%s:%s found" % params.group(1,3,5))
                 return
-            
-            # Galaxy
-            else:
-                galaxy = Galaxy.load(*params.group(1,3))
-                if galaxy is None:
-                    message.reply("No galaxy with coords %s:%s" % params.group(1,3))
-                    return
-                message.reply(str(galaxy))
-                return
-            
+            message.reply(str(planet))
+            return
         
-        # User or Alliance
+        # Galaxy
         else:
-            alliance = Alliance.load(params.group(1)) if params.group(1) is not None else None
-            # Alliance
-            if alliance is not None:
-                message.reply(str(alliance))
+            galaxy = Galaxy.load(*params.group(1,3))
+            if galaxy is None:
+                message.reply("No galaxy with coords %s:%s" % params.group(1,3))
                 return
-            
-            # User
-            if params.group(1) is None:
-                message.reply(str(self.get_user_planet(user)))
+            message.reply(str(galaxy))
+            return
+    
+    @route(r"(?:\s(\S+))?")
+    def user_alliance(self, message, user, params):
+        alliance = Alliance.load(params.group(1)) if params.group(1) is not None else None
+        # Alliance
+        if alliance is not None:
+            message.reply(str(alliance))
+            return
+        
+        # User
+        if params.group(1) is None:
+            message.reply(str(self.get_user_planet(user)))
+            return
+        elif not self.is_user(user):
+            raise PNickParseError
+        elif not user.is_member():
+            message.reply("No alliance matching '%s' found" % (params.group(1),))
+            return
+        else:
+            lookup = User.load(params.group(1), exact=False)
+            if lookup is None:
+                message.reply("No alliance or user matching '%s' found" % (params.group(1),))
                 return
-            elif not self.is_user(user):
-                raise PNickParseError
-            elif not user.is_member():
-                message.reply("No alliance matching '%s' found" % (params.group(1),))
+            elif lookup.planet is None:
+                message.reply("User %s has not entered their planet details" % (lookup.name,))
                 return
             else:
-                lookup = User.load(params.group(1), exact=False)
-                if lookup is None:
-                    message.reply("No alliance or user matching '%s' found" % (params.group(1),))
-                    return
-                elif lookup.planet is None:
-                    message.reply("User %s has not entered their planet details" % (lookup.name,))
-                    return
-                else:
-                    message.reply(str(lookup.planet))
-                    return
+                message.reply(str(lookup.planet))
+                return
