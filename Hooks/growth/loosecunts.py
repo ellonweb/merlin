73bbda5685a5752acf1cd0504591144c288800fa
ellonweb <ellonweb@hotmail.com>
2010-02-24 01:10:34 +0000

    2010 license

--- Hooks/growth/loosecunts.py
+++ Hooks/growth/loosecunts.py
@@ -1,5 +1,5 @@
 # This file is part of Merlin.
-# Merlin is the Copyright (C)2008-2009 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
+# Merlin is the Copyright (C)2008,2009,2010 of Robin K. Hansen, Elliot Rosemarine, Andreas Jacobsen.
 
 # Individual portions may be copyright by individual contributors, and
 # are included in this collective work with permission of the copyright
