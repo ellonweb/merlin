e320f14a63a55840a22fcd8724d15734255685ba
ellonweb <ellonweb@hotmail.com>
2010-03-25 00:03:49 +0000

    racism for galaxies, JDD's idea

--- Hooks/intel/racism.py
+++ Hooks/intel/racism.py
@@ -22,15 +22,15 @@
 from sqlalchemy.sql import asc
 from sqlalchemy.sql.functions import count, sum
 from Core.db import session
-from Core.maps import Planet, Alliance, Intel
+from Core.maps import Galaxy, Planet, Alliance, Intel
 from Core.loadable import loadable, route
 
 class racism(loadable):
-    """Shows averages for each race matching a given alliance in intel."""
-    usage = " <alliance>"
+    """Shows averages for each race matching a given alliance in intel or for a galaxy."""
+    usage = " <alliance> | <x:y>"
     
     @route(r"(\S+)", access = "member")
-    def execute(self, message, user, params):
+    def intel_alliance(self, message, user, params):
         
         alliance = Alliance.load(params.group(1))
         if alliance is None:
@@ -49,11 +49,33 @@ class racism(loadable):
         if len(result) < 1:
             message.reply("No planets in intel match alliance %s"%(alliance.name,))
             return
+        
+        self.execute(message, alliance.name, result)
+    
+    @route(loadable.coord, access = "member")
+    def galaxy(self, message, user, params):
+        galaxy = Galaxy.load(*params.group(1,3))
+        if galaxy is None:
+            message.alert("No galaxy with coords %s:%s" % params.group(1,3))
+            return
+        
+        Q = session.query(sum(Planet.value), sum(Planet.score),
+                          sum(Planet.size), sum(Planet.xp),
+                          count(), Planet.race)
+        Q = Q.filter(Planet.galaxy == galaxy)
+        Q = Q.filter(Planet.active == True)
+        Q = Q.group_by(Planet.race)
+        Q = Q.order_by(asc(Planet.race))
+        result = Q.all()
+        
+        self.execute(message, "%s:%s" % (galaxy.x, galaxy.y,), result)
+    
+    def execute(self, message, target, result):
         prev=[]
         for value, score, size, xp, members, race in result:
             reply="%s %s Val(%s)" % (members,race,self.num2short(value/members),)
             reply+=" Score(%s)" % (self.num2short(score/members),)
             reply+=" Size(%s) XP(%s)" % (size/members,self.num2short(xp/members),)
             prev.append(reply)
-        reply="Demographics for %s: "%(alliance.name,)+ ' | '.join(prev)
+        reply="Demographics for %s: "%(target,)+ ' | '.join(prev)
         message.reply(reply)
