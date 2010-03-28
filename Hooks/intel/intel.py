ellonweb/merlin

commit dbc7541732bef1c908decd63ad4fbb0898857038

updated intel hooks

Hooks/intel/intel.py
@@ -22,18 +22,17 @@
 from sqlalchemy.sql import asc
 from Core.db import session
 from Core.maps import Galaxy, Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
 options = ['alliance', 'nick', 'fakenick', 'defwhore', 'covop', 'scanner', 'dists', 'bg', 'gov', 'relay', 'reportchan', 'comment']
 
-@loadable.module("member")
 class intel(loadable):
     """View or set intel for a planet. Valid options: """
     __doc__ += ", ".join(options)
-    usage = " x.y[.z] [option=value]+"
-    paramre = loadable.coordre
+    usage = " <x.y[.z]> [option=value]+"
     
-    def execute(self, message, user, params):
+    @route(loadable.coord+r"\s*$", access = "member")
+    def view_intel(self, message, user, params):
         
         if params.group(5) is None:
             galaxy = Galaxy.load(*params.group(1,3))
@@ -64,6 +63,18 @@ class intel(loadable):
                 message.reply("No information stored for %s:%s" % (galaxy.x, galaxy.y,))
             return
         
+        planet = Planet.load(*params.group(1,3,5))
+        if planet is None:
+            message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
+            return
+        
+        if str(planet.intel):
+            message.reply("Information stored for %s:%s:%s -%s"% (planet.x, planet.y, planet.z, str(planet.intel),))
+        else:
+            message.reply("No information stored for %s:%s:%s"% (planet.x, planet.y, planet.z,))
+    
+    @route(loadable.planet_coord+r"\s+(\S.*)", access = "member")
+    def set_intel(self, message, user, params):
         planet = Planet.load(*params.group(1,3,5))
         if planet is None:
             message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
@@ -101,7 +112,7 @@ class intel(loadable):
             if opt == "comment":
                 planet.intel.comment = message.get_msg().split("comment=")[1]
         session.commit()
-        if planet.intel:
+        if str(planet.intel):
             message.reply("Information stored for %s:%s:%s -%s"% (planet.x, planet.y, planet.z, str(planet.intel),))
         else:
             message.reply("No information stored for %s:%s:%s"% (planet.x, planet.y, planet.z,))

Hooks/intel/bumchums.py
@@ -19,25 +19,23 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from sqlalchemy.sql.functions import count
 from Core.db import session
 from Core.maps import Galaxy, Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module("member")
 class bumchums(loadable):
     """Pies"""
-    usage = " alliance number"
-    paramre = re.compile(r"\s(\S+)(?:\s(\d+))?")
+    usage = " <alliance> [number]"
     
+    @route(r"\s+(\S+)(?:\s+(\d+))?", access = "member")
     def execute(self, message, user, params):
         
         alliance = Alliance.load(params.group(1))
         if alliance is None:
             message.reply("No alliance matching '%s' found"%(params.group(1),))
             return
-        bums = int(params.group(2) or 1)
+        bums = int(params.group(2) or 2)
         Q = session.query(Galaxy.x, Galaxy.y, count())
         Q = Q.join(Galaxy.planets)
         Q = Q.join(Planet.intel)

Hooks/intel/info.py
@@ -19,19 +19,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from sqlalchemy.sql import desc
 from sqlalchemy.sql.functions import count, sum
 from Core.db import session
 from Core.maps import Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module("member")
 class info(loadable):
     """Alliance information (All information taken from intel, for tag information use the lookup command)"""
-    usage = " alliance"
-    paramre = re.compile(r"\s(\S+)")
+    usage = " <alliance>"
     
+    @route(r"\s+(\S+)", access = "member")
     def execute(self, message, user, params):
         
         alliance = Alliance.load(params.group(1))

Hooks/intel/racism.py
@@ -19,19 +19,17 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from sqlalchemy.sql import asc
 from sqlalchemy.sql.functions import count, sum
 from Core.db import session
 from Core.maps import Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module("member")
 class racism(loadable):
     """Shows averages for each race matching a given alliance in intel."""
-    usage = " alliance"
-    paramre = re.compile(r"\s(\S+)")
+    usage = " <alliance>"
     
+    @route(r"\s+(\S+)", access = "member")
     def execute(self, message, user, params):
         
         alliance = Alliance.load(params.group(1))

Hooks/intel/search.py
@@ -19,18 +19,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from sqlalchemy import and_, or_
 from Core.db import session
 from Core.maps import Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module("member")
 class search(loadable):
     """Search for a planet by alliance or nick."""
-    usage = " alliance|nick"
-    paramre = re.compile(r"\s(\S+)")
+    usage = " <alliance|nick>"
     
+    @route(r"\s+(\S+)", access = "member")
     def execute(self, message, user, params):
         
         param = "%"+params.group(1)+"%"

Hooks/intel/spam.py
@@ -19,18 +19,16 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
-import re
 from sqlalchemy.sql import asc
 from Core.db import session
 from Core.maps import Planet, Alliance, Intel
-from Core.loadable import loadable
+from Core.loadable import loadable, route
 
-@loadable.module("member")
 class spam(loadable):
     """Spam alliance coords"""
-    usage = " alliance"
-    paramre = re.compile(r"\s(\S+)")
+    usage = " <alliance>"
     
+    @route(r"\s+(\S+)", access = "member")
     def execute(self, message, user, params):
         
         alliance = Alliance.load(params.group(1), active=False)

commit f0a0b78096b94135b46421143364518e7fa1788d

fixed intel bug introduced in dbc7541

Hooks/intel/intel.py
@@ -68,7 +68,7 @@ class intel(loadable):
             message.alert("No planet with coords %s:%s:%s" % params.group(1,3,5))
             return
         
-        if str(planet.intel):
+        if planet.intel and str(planet.intel):
             message.reply("Information stored for %s:%s:%s -%s"% (planet.x, planet.y, planet.z, str(planet.intel),))
         else:
             message.reply("No information stored for %s:%s:%s"% (planet.x, planet.y, planet.z,))