18e12959678fb0817a49496fcd82817a0b60ca54
ellonweb <ellonweb@hotmail.com>
2010-05-30 14:07:24 +0100

    hammertime

--- Hooks/ships/stop.py
+++ Hooks/ships/stop.py
@@ -29,6 +29,10 @@ class stop(loadable):
     """Calculates the required defence to the specified number of ships"""
     usage = " <number> <ship> [t1|t2|t3]"
     
+    @route(r"hammertime")
+    def hammertime(self, message, user, params):
+        message.reply("Can't touch this!")
+    
     @route(r"(\d+(?:\.\d+)?[km]?)\s+(\w+)(?:\s+(t1|t2|t3))?")
     def execute(self, message, user, params):
         
