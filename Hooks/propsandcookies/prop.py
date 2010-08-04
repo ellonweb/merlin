52bc559e70543d6f51b2b9732b3c500e0bdaa81d
ellonweb <ellonweb@hotmail.com>
2010-08-04 16:50:42 +0100

    bugfix to the bugfix (ellonweb fails #8352)

--- Hooks/propsandcookies/prop.py
+++ Hooks/propsandcookies/prop.py
@@ -98,7 +98,9 @@ class prop(loadable):
             return
         
         old_vote = prop.votes.filter(Vote.voter==user).first()
-        session.expunge(old_vote)
+        if old_vote is not None:
+            session.expunge(old_vote)
+        
         prop.votes.filter(Vote.voter==user).delete()
         prop.votes.append(Vote(voter=user, vote=vote, carebears=user.carebears))
         session.commit()
