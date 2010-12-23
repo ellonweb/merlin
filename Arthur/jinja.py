c307959f0e3439c06210f49ed5596faa5d0dc1b4
ellonweb <ellonweb@hotmail.com>
2010-12-23 01:06:47 +0000

    jinja 2.6 required

--- Arthur/jinja.py
+++ Arthur/jinja.py
@@ -19,6 +19,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  
+import sys
+import jinja2
+if not 6.0 <= float(jinja2.__version__[2:5]):
+    sys.exit("Jinja2 2.6.0+ Required")
+
 from jinja2 import Environment, FileSystemLoader
 
 from Arthur.templatetags.url import URLReverserExtension
